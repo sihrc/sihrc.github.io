93d3dfaa8cb1db52760023ca9edb38329f40e3d2
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-04 22:06:55 -0500

    updated the project button

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,25 +24,38 @@ function getWindowHeight(){
 //Set the position of the footer
 function setBottomBar(){
 	//Projected Footer Position
-	var footerHeight = getWindowHeight() - $('#footerbar').height() - 20;
+	var footerHeight = getWindowHeight() - $('#projects').height() - 74;
 	//Content Position
 	var profileHeight = $('#profile').height() + $('#profile').offset().top + 30;
 	//Choosing which to use
 	if (footerHeight > profileHeight)
-		$('#footerbar').offset({left:0,top:footerHeight});
+		$('#projects').offset({left:0,top:footerHeight});
 	else 
-		$('#footerbar').offset({left:0,top:profileHeight});
+		$('#projects').offset({left:0,top:profileHeight});
 }
 
+//Set Project Content
+function setProjectContent(){
+	var projectheight = $('#projects').offset().top + 70;
+	$('#projectcontent').offset({left:0,top:projectheight});
+}
 //Set the holder alignment
 function setHolder(){
 	$('#dividor').width(.6*$('#holder').width());
 }
 
+function setProjectAnimation(){
+	$('#footerbar').click(function(e){
+		//Make Animation
+	});
+}
+
 //What to run when document is ready
 $(document).ready(function(){
 	setBottomBar();
+	setProjectContent()
 	setHolder();
+	setProjectAnimation()
 });
 
 $(window).resize(function(){
